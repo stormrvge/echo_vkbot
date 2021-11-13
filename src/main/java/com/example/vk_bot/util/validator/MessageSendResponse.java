8cb54222a6d7fc0528323848ce8423e0fa02d66c
Andrey Svistukhin <andrey@enhancera.com>
2021-11-13 04:59:25 +0300

    Added logger to MessageSendResponse.

--- src/main/java/com/example/vk_bot/util/validator/MessageSendResponse.java
+++ src/main/java/com/example/vk_bot/util/validator/MessageSendResponse.java
@@ -3,6 +3,8 @@ package com.example.vk_bot.util.validator;
 import com.example.vk_bot.dto.MessageAnswer;
 import com.example.vk_bot.util.exceptions.InvalidRequestException;
 import java.util.Objects;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
@@ -12,11 +14,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class MessageSendResponse {
 
+  private static final Logger log = LoggerFactory.getLogger(MessageSendResponse.class);
+
   public void validate(ResponseEntity<MessageAnswer> response) {
     long errorCode = Objects.requireNonNull(response.getBody()).getErrorCode();
     if (errorCode != 0) {
       MessageAnswer answerBody = Objects.requireNonNull(response.getBody());
       String errorMsg = Objects.requireNonNull(answerBody.getErrorMsg());
+      log.error("Error during validation request to VK API. Error code: {}. Message: {}", errorCode,
+          errorMsg);
       throw new InvalidRequestException(errorMsg);
     }
   }
