2e2be853d20d708e71834adedfe85b477d7a656b
Andrey Svistukhin <andrey@enhancera.com>
2021-11-13 17:04:33 +0300

    added if condition on checking is callback empty in CallbackAPIServiceImpl.

--- src/main/java/stormrage/vk_bot/service/CallbackAPIServiceImpl.java
+++ src/main/java/stormrage/vk_bot/service/CallbackAPIServiceImpl.java
@@ -1,5 +1,6 @@
 package stormrage.vk_bot.service;
 
+import java.security.InvalidParameterException;
 import stormrage.vk_bot.dto.Callback;
 import stormrage.vk_bot.dto.MessageOut;
 import stormrage.vk_bot.entity.MessageIn;
@@ -27,6 +28,10 @@ public class CallbackAPIServiceImpl implements CallbackAPIService {
   private final VkApiProperties vkApiProperties;
 
   public String handleCallback(Callback callback) {
+    if (callback.isEmpty()) {
+      log.info("An empty callback came to the server");
+      throw new InvalidParameterException("An empty callback came to the server.");
+    }
     validateCallbackSecret(callback.getSecret());
     switch (callback.getType()) {
       case MESSAGE_NEW:
@@ -36,7 +41,7 @@ public class CallbackAPIServiceImpl implements CallbackAPIService {
         return vkApiProperties.getConfirmation();
       default:
         log.info("An unsupported request type came in.");
-        throw new UnsupportedTypeOfCallback("Service doesn't support this type of callback!");
+        throw new UnsupportedTypeOfCallback("Service doesn't support this type of callback.");
     }
   }
 
